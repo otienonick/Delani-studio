e564b8d51ae302a4467ec41c2a06a16d3c978ac3
otienonick <nick.odongi@student.moringaschool.com>
2021-07-23 14:38:28 +0300

    added toggle effect to images in what we do section

---
+++ script.js
@@ -0,0 +1,35 @@
+//What we do section 
+
+$(document).ready(function() {
+    $('#design-icon').click(function() {
+        $('.hidden1').show();
+        $('#design-icon').hide();
+    });
+    $('.hidden1').click(function() {
+        $('#design-icon').show();
+        $('.hidden1').hide();
+
+    });
+});
+$(document).ready(function() {
+    $('#dev-icon').click(function() {
+        $('.hidden2').show();
+        $('#dev-icon').hide();
+    });
+    $('.hidden2').click(function() {
+        $('#dev-icon').show();
+        $('.hidden2').hide();
+
+    });
+});
+$(document).ready(function() {
+    $('#product-icon').click(function() {
+        $('.hidden3').show();
+        $('#product-icon').hide();
+    });
+    $('.hidden3').click(function() {
+        $('#product-icon').show();
+        $('.hidden3').hide();
+
+    });
+});
